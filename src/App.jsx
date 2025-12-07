7f51defb43017f6a4fc916260721d854fe1f9362
Rohan Verma <thechampionsmail@gmail.com>
2025-12-06 16:11:22 -0800

    Remove photo metadata

--- src/App.jsx
+++ src/App.jsx
@@ -38,7 +38,7 @@ export default function App() {
     <>
       <ScrollToTop />
       <AppShell>
-        <AnimatePresence initial={false} mode="wait">
+        <AnimatePresence mode="wait">
           <Routes location={location} key={location.pathname}>
             <Route index element={<About />} />
             <Route path="/experience" element={<Experience />} />
